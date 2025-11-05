3d4194b3387d5fcaedb5ad8f1d53418e5939ad23
Samin Yasar <yasarsamin57@gmail.com>
2025-11-05 12:10:09 +0600

    dev: splash screen layout added

--- app/splash/_layout.tsx
+++ app/splash/_layout.tsx
@@ -1,20 +1,13 @@
 import { Stack } from 'expo-router';
 import 'nativewind';
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar } from 'react-native';
 import ToastManager from 'toastify-react-native';
 import '../../global.css';
 
 export default function SplashLayout() {
-  const colorScheme = useColorScheme();
-  const isDark = colorScheme === 'dark';
-
   return (
     <>
-      <StatusBar
-        backgroundColor={isDark ? '#000' : '#fff'}
-        translucent={true}
-        barStyle={isDark ? 'light-content' : 'dark-content'}
-      />
+      <StatusBar backgroundColor="transparent" translucent={true} barStyle="light-content" />
       <Stack
         screenOptions={{
           headerShown: false,
