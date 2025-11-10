63e9b615eabe843152bd39de86146015b42cf563
Samin Yasar <yasarsamin57@gmail.com>
2025-11-10 10:34:47 +0600

    dev: auth layout added

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+import 'nativewind';
+import { StatusBar } from 'react-native';
+import ToastManager from 'toastify-react-native';
+import '../../global.css';
+
+export default function AuthLayout() {
+  return (
+    <>
+      <StatusBar backgroundColor="transparent" translucent={true} barStyle="light-content" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack>
+      <ToastManager />
+    </>
+  );
+}
